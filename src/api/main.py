f56f018a88e8d2dfd1ad7caa01dad1325ecb68a3
vishal267 <vishal267@ymail.com>
2025-08-15 23:19:26 +0530

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -5,9 +5,6 @@ from schemas import HousePredictionRequest, PredictionResponse
 from prometheus_fastapi_instrumentator import Instrumentator
 from prometheus_client import start_http_server
 import threading
-# Start Prometheus metrics server on port 9100 in a background thread
-def start_metrics_server():
-start_http_server(9100)
 
 # Initialize FastAPI app with metadata
 app = FastAPI(
@@ -39,6 +36,11 @@ app.add_middleware(
 )
 # Initialize and instrument Prometheus metrics
 Instrumentator().instrument(app).expose(app) 
+
+# Start Prometheus metrics server on port 9100 in a background thread
+def start_metrics_server():
+start_http_server(9100)
+threading.Thread(target=start_metrics_server, daemon=True).start()
 
 # Health check endpoint
 @app.get("/health", response_model=dict)
